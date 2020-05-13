33a009c73b77b1f2cfcbfa5b9c12f4b287dd37f2
drapodi <33332410+drapodi@users.noreply.github.com>
2020-05-13 12:39:02 -0400

    updated DB credentials and such

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,8 +1,8 @@
 heroku_config: dict = {
-    'user': 'fryclimmwivdsj',
-    'password': '910ee3582b5b2ee1f7654ce0f9a6783665a195eb89759facc35009ef9611dd48',
-    'host': 'ec2-54-81-37-115.compute-1.amazonaws.com',
-    'dbname': 'dsgbl3f9vgbh2',
+    'user': 'ojvthjvchvssiw',
+    'password': 'd6a1d95b49c48f71a26d43f4b3856488578ea4e4da495dc76dbc33cbc81c1330',
+    'host': 'ec2-18-232-143-90.compute-1.amazonaws.com',
+    'dbname': 'd973ipbuimjhj',
     'port': '5432',
     'uri': 'postgres://fryclimmwivdsj:910ee3582b5b2ee1f7654ce0f9a6783665a195eb8975' +
            '9facc35009ef9611dd48@ec2-54-81-37-115.compute-1.amazonaws.com:5432/dsgbl3f9vgbh2'
