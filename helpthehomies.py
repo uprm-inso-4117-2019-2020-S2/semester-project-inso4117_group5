4825f11f4ed26da63f4840f50dbd25a6b561e8af
Javier Ortiz <javier.ortiz13@upr.edu>
2020-05-17 19:11:52 -0400

    fixed directory error

--- helpthehomies.py
+++ helpthehomies.py
@@ -8,8 +8,8 @@ import os
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 # Apply CORS to this app
-template_dir = "../templates"
-static_dir = "../static"
+template_dir = "./templates"
+static_dir = "./static"
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 app.secret_key = '5791628bb0b13ce0c676dfde280ba245'
 app.config['JSON_SORT_KEYS'] = False  # This makes jsonify NOT sort automatically.
