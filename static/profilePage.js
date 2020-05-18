db2d62cb562d80eeb2aaf4bfafeb7cb250c6d628
Hector Dael <hectordael@Hectors-MacBook-Pro.local>
2020-05-17 23:10:13 -0400

    Finished profilePage. Need to integrate with backend.

--- static/profilePage.js
+++ static/profilePage.js
@@ -1,35 +1,113 @@
-let username = "John Marston";
-let pendingTickets = document.getElementById('pending-ticket-container');
-let acceptedTickets = document.getElementById('accepted-ticket-container')
+// AUTHOR: HECTOR JIMENEZ MERCADO
 
-let tickets = 7;
+let username = "John Marston";
+let pendingTicketsContainer = document.getElementById('pending-ticket-container');
+let acceptedTicketsContainer = document.getElementById('accepted-ticket-container')
+
+// Mock Data
+var pendingTickets =  
+[
+    {
+        "title": "Water",
+        "location": "San Juan",
+        "quantity": 21,
+        "provider": "John Balbuena"
+    }, 
+
+    {
+        "title": "Rice",
+        "location": "Arecibo",
+        "quantity": 10,
+        "provider": "Maria Magdalena"
+    },
+
+    {
+        "title": "Pottery",
+        "location": "Mayaguez",
+        "quantity": 7,
+        "provider": "Beatrice Muniz"
+    },
+
+    {
+        "title": "First Aid Kits",
+        "location": "Quebradillas",
+        "quantity": 20,
+        "provider": "Heriberto Velez"
+    },
+
+    {
+        "title": "Mano de Obra",
+        "location": "Jayuya",
+        "quantity": null,
+        "provider": "Jose Delgado"
+    },
+
+    {
+        "title": "Water",
+        "location": "San Juan",
+        "quantity": 21,
+        "provider": "John Balbuena"
+    }
+];
+
+var acceptedTickets = [
+    {
+        "title": "Mano de Obra",
+        "location": "Jayuya",
+        "quantity": null,
+        "provider": "Jose Delgado"
+    },
+
+    {
+        "title": "Pottery",
+        "location": "Mayaguez",
+        "quantity": 7,
+        "provider": "Beatrice Muniz"
+    },
+
+    {
+        "title": "Water",
+        "location": "San Juan",
+        "quantity": 21,
+        "provider": "John Balbuena"
+    }, 
+    
+]
 
 function load() {
     document.getElementById("header-register").innerHTML += username;
-    
+    loadPendingTickets();
     loadAcceptedTickets();
 }
 
 function loadPendingTickets() {
-    for(let i=0; i<tickets; i++){
-        console.log(i);
-        let ticket = document.createElement("div");
-        ticket.id = 'ticket';
-        
-        pendingTickets.appendChild(ticket);
+    for(let ticket of pendingTickets){
+        let newTicket = document.createElement("div");
+        newTicket.id = 'ticket';
+
+        newTicket.innerHTML= 
+            createTicket(ticket.title, ticket.location, ticket.quantity, ticket.provider)
+
+        pendingTicketsContainer.appendChild(newTicket);
     }
 }
 
 function loadAcceptedTickets(){
-    for(let i=0; i<tickets-3; i++){
-        console.log(i);
-        let ticket = document.createElement("div");
-        ticket.id = 'ticket';
-        ticket.innerHTML = `                <h3 style="color: whitesmoke;">Beerbongs</h3>
-        <p>Location: San Juan</p>
-        <p style="color: red;">Quantity: x13</p>
-        <p>Provider: Ben Stiller</p>`
+    for(let ticket of acceptedTickets){
+        let newTicket = document.createElement("div");
+        newTicket.id = 'ticket';
 
-        acceptedTickets.appendChild(ticket);
+        newTicket.innerHTML = 
+            createTicket(ticket.title, ticket.location, ticket.quantity, ticket.provider)
+
+        acceptedTicketsContainer.appendChild(newTicket);
     }
+}
+
+function createTicket(title, location, quantity, provider) {    
+    return `                
+    <h4 style="color: whitesmoke;">${title}</h4>
+    <p>Location: ${location}</p>
+    <p style="color: red;">Quantity: x${quantity}</p>
+    <p>Provider: ${provider}</p>`
 }
